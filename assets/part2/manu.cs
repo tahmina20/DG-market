e4bf98cd0cb2f8a9f48560faf015396b552053f4
tahmina20 <you@example.com>
2020-09-05 08:23:02 +0600

    file upload

---
+++ assets/part2/manu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manu : MonoBehaviour {
+
+	public void GoToMainMenu()
+	{
+		Application.LoadLevel ("loading");
+	}
+
+	public void GoToARCamera()
+	{
+		Application.LoadLevel ("road 3");
+
+	}
+		
+
+	public void ExitApplication()
+	{
+		Application.Quit ();
+	}
+}
---
+++ assets/part2/menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+	public void GoToMainMenu()
+	{
+		Application.LoadLevel ("loading");
+	}
+
+	public void GoToARCamera()
+	{
+		Application.LoadLevel ("level01");
+
+	}
+
+	public void GoTOAbout()
+	{
+		Application.LoadLevel ("about");
+	}
+
+	public void ExitApplication()
+	{
+		Application.Quit ();
+	}
+}
